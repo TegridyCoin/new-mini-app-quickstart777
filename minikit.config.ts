12598b20e511c6c078cc7a9b94fb278ecfe447ed
TegridyCoin <chicagobandenproduction@gmail.com>
2025-11-23 15:34:07 +0200

    Update minikit.config.ts

--- minikit.config.ts
+++ minikit.config.ts
@@ -9,14 +9,14 @@ const ROOT_URL =
  */
 export const minikitConfig = {
   accountAssociation: {
-    header: "",
-    payload: "",
-    signature: ""
+    header: "eyJmaWQiOjY0NDc2MSwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDU2QjE2ODIxYWFBQjU4RUNDNkYyODIyYzEwRTU1ZjgzNjk3MTI2NEUifQ",
+    payload: "eyJkb21haW4iOiJuZXctbWluaS1hcHAtcXVpY2tzdGFydDc3Ny52ZXJjZWwuYXBwIn0",
+    signature: "/VnQ4oiTG2yTq/V+lFdS3apSDi3tNkgHusyycfKhArQAHSQmsp5FGMZQ/vvL/9cq53wt6QnWlaZxB4v/NQ5rEBs="
   },
   miniapp: {
     version: "1",
-    name: "Cubey", 
-    subtitle: "Your AI Ad Companion", 
+    name: "Cubey",
+    subtitle: "Your AI Ad Companion",
     description: "Ads",
     screenshotUrls: [`${ROOT_URL}/screenshot-portrait.png`],
     iconUrl: `${ROOT_URL}/blue-icon.png`,
@@ -26,11 +26,10 @@ export const minikitConfig = {
     webhookUrl: `${ROOT_URL}/api/webhook`,
     primaryCategory: "social",
     tags: ["marketing", "ads", "quickstart", "waitlist"],
-    heroImageUrl: `${ROOT_URL}/blue-hero.png`, 
+    heroImageUrl: `${ROOT_URL}/blue-hero.png`,
     tagline: "",
     ogTitle: "",
     ogDescription: "",
     ogImageUrl: `${ROOT_URL}/blue-hero.png`,
   },
 } as const;
-
